8920c04f855098e38f30654ab1f3788adf041c69
uvenven <fadilefd1102@gmail.com>
2026-09-17 10:23:23 +0700

    feat: add LoginView component with employee and admin authentication

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -213,3 +213,16 @@ const handleLogin = async () => {
   }
 };
 </script>
+
+<style scoped>
+/* Menghilangkan panah atas/bawah pada input type="number" di berbagai browser */
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
